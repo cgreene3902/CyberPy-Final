1c4d150af7014a6aac2992b597ce3d1e527f1612
cgreene3902 <cgreene3902@gmail.com>
2019-11-06 13:49:06 -0500

    testfile.py

---
+++ testfile.py
@@ -0,0 +1,35 @@
+"""
+This is just a file that will be 
+used to test different things from 
+the pygame module
+"""
+import pygame
+
+# main function
+def main():
+
+    # initialize pygame module
+    pygame.init()
+    # loads and sets the logo
+    logo = pygame.image.load("")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("")
+
+    # creates a surface on screen that is 240 x 180 in resolution
+    screen = pygame.display.set_mode((240,180))
+
+    # variable to control main loop
+    running = True
+
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit main loop
+                running = False
+
+if __name__=="__main__":
+    # call main function
+    main()
