da497d22435cddfe2cda88a4693fbaf374cd2425
cgreene3902 <cgreene3902@gmail.com>
2019-11-06 20:00:55 -0500

    testfile.py

--- testfile.py
+++ testfile.py
@@ -4,19 +4,29 @@ used to test different things from
 the pygame module
 """
 import pygame
-
+import os
 # main function
 def main():
 
     # initialize pygame module
     pygame.init()
     # loads and sets the logo
-    logo = pygame.image.load('Images/logo.png')
+    logo = pygame.image.load(os.path.join('Images', 'logo.png'))
+    
+    image1 = pygame.image.load(os.path.join('Images', '1.png'))
+    
+
     pygame.display.set_icon(logo)
     pygame.display.set_caption('test')
 
     # creates a surface on screen that is 240 x 180 in resolution
     screen = pygame.display.set_mode((240,180))
+    r, g, b = 100, 100, 100
+    screen.fill((r,g,b))
+    # copies image '1.png' to the screen
+    screen.blit(image1, (50,50))
+    # displays the image '1.png' onto the screen
+    pygame.display.flip()
 
     # variable to control main loop
     running = True
