b0a812cf81ea66a56bdf3ea8f10a61ad4d95d67d
cgreene3902 <cgreene3902@gmail.com>
2019-11-12 22:49:06 -0500

    changed the movement in testfile3.py

--- testfile3.py
+++ testfile3.py
@@ -6,24 +6,37 @@ import os
 import random
 ####
 class App(object):
+    
     def __init__(self):
         self._running = True
-        self._displ_surf = None
+        self._screen = None
         self.size = self.width, self.height = 640, 480
-
+    
     def on_init(self): # initializes pygame modules and creates main display
         pygame.init()
-        self._displ_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        global _screen
+        _screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._screen = _screen
         self._running = True
-        self._bg = pygame.Surface(self._displ_surf.get_size()).convert()
+        global _bg
+        _bg = pygame.Surface(self._screen.get_size()).convert()
+        self._bg = _bg
+        self._bg.fill((50,50,50))
         self.clock = pygame.time.Clock()
-        self.fps = 60
+        self.fps = 65
         self.seconds = 0.0
         self.font = pygame.font.SysFont('mono', 12, bold=True)
+        self.y = 100
+        self.x = 615
         self.x1 = 1
+        self.x2, self.y2 = 0, 0
+        
+        pygame.draw.rect(self._bg, (255,255,255), ((self.width/2),0,1,480))
+        pygame.draw.rect(self._bg, (255,255,255), (0,(self.height/2),640,1))
         #pygame.draw.rect(self._bg, (200,200,255), (0,380,self.width,380))
         
         
+        self._screen.blit(self._bg, (0, 0))
     
     def on_event(self, event): # events to called by the event handler
         if event.type == pygame.QUIT:
@@ -31,8 +44,16 @@ class App(object):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self._running = False
+            
+            elif event.key == pygame.K_1:
+                self.fps = 100
+            elif event.key == pygame.K_r:
+                self.fps = 65
+
+
     
     def on_loop(self): # controls changes in the game world
+        
         ####
         ms = self.clock.tick(self.fps)
         self.seconds += ms / 1000.0
@@ -41,17 +62,17 @@ class App(object):
         ####
         pygame.display.set_caption('FPS: %.2f' % (self.clock.get_fps()))
         
-        # testing out object movement
-        self.x1 += 1.5 * 0.75
-        pygame.draw.rect(self._bg, (255,255,255), (0,380,self.x1,480))
-        
+    
+    
     def on_render(self): # prints out screen graphics
+        
 
         surface = self.font.render(self.text1, True, (0, 160, 185))
-        self._displ_surf.blit(surface, (15 // 2, 15 // 2))
+        self._screen.blit(surface, (15 // 2, 15 // 2))
         
-        pygame.display.flip()
-        self._displ_surf.blit(self._bg, (0, 0))
+
+        pygame.display.update()
+        self._screen.blit(self._bg, (0, 0))
         
         
     def on_cleanup(self):
@@ -64,30 +85,50 @@ class App(object):
         while(self._running): # main loop init
             for event in pygame.event.get(): # event handler 
                 self.on_event(event) # calls event in on_event
+            player = App.player_movement()
+            player.draw(self._screen)
+            pygame.display.update()
             self.on_loop() # 
             self.on_render() # calls objects in on_render to be rendered
         self.on_cleanup()
 ####
-class shapes(object):
-    """This is where shape based objects will be defined
-    """
-    def __init__(self, x, y, speed_x=1, speed_y=1):
+    class player_movement():
+        
+        def __init__(self):
+            self.moving_rect1 = pygame.draw.rect(_screen, (255,255,255), (615,100,20,280))
 
-        self.x = x
-        self.y = y
-        self.speed_x = speed_x
-        #self.speed_y = speed_y
-    
-    @property
-    def max_x(self):
+            # handle_keys
+        def handle_keys(self):
+            pressedkeys = pygame.key.get_pressed()
+            if pressedkeys[pygame.K_a]:
+                self.draw_rect(-1,0)
+            if pressedkeys[pygame.K_d]:
+                self.draw_rect(1,0)
+            if pressedkeys[pygame.K_w]:
+                self.draw_rect(0,1)
+            if pressedkeys[pygame.K_s]:
+                self.draw_rect(0,-1) 
+        
 
-        return self.x
+        def draw_rect(self,x,y):
+            _screen.fill((255,255,255))
+            self.moving_rect1 = self.moving_rect1.move(x, y); pygame.draw.rect(self._screen, (255,255,255), self.moving_rect1)
+            pygame.display.update()
+            
+        def draw(self,surface):
+            pygame.draw.rect(_bg, (255,255,255), (615,100,20,280))
 
-    def rel_move(self, dx, dy):
-        self.x += dx
-        self.y += dy
 
-####
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     theApp = App()
     theApp.on_execute()
